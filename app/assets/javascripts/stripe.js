664613839f94e0bd8f95b3ae99e6c11e3303259e
Sinead <sineadfox24@gmail.com>
2016-07-22 13:06:08 +0000

    Added pg and rails_12factor gems for Heroku

--- app/assets/javascripts/stripe.js
+++ app/assets/javascripts/stripe.js
@@ -1,43 +1,42 @@
 
 
-<head>
-<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+//<head>
+//<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 
-<script>
-  Stripe.setPublishableKey(' pk_test_Z9jqqrx4a0i2osf6Q4BhSTVP');
-</script>
+//<script>
+  //Stripe.setPublishableKey(' pk_test_Z9jqqrx4a0i2osf6Q4BhSTVP');
+//</script>
 
-</head>
+//</head>
 
 
 
-function stripeResponseHandler(status, response) {
+//function stripeResponseHandler(status, response) {
  
-    if (response.error) {
-        (".payment-errors").text(response.error.message);
-    } 
-    else {
-        var form$ = ("#stripe-form");
-        var token = response['id'];
-        form$.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
-        form$.get(0).submit();
-    }
-}
+  //  if (response.error) {
+  //      (".payment-errors").text(response.error.message);
+    //} 
+    //else {
+      //  var form$ = ("#stripe-form");
+        //var token = response['id'];
+        //form$.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
+        //form$.get(0).submit();
+    //}
+//}
  
-(document).ready(function() {
-  ("#stripe-form").submit(function(event) {
-    ('.submit').attr("disabled", "disabled");
+//(document).ready(function() {
+  //("#stripe-form").submit(function(event) {
+    //('.submit').attr("disabled", "disabled");
  
-    $Stripe.createToken({
-        number: $('.credit-number').val(),
-        cvc: $('.credit-security').val(),
-        exp_month: $('.card-expiry-month').val(),
-        exp_year: $('.card-expiry-year').val()
-    }, stripeResponseHandler);
+    //$Stripe.createToken({
+      //  number: $('.credit-number').val(),
+        //cvc: $('.credit-security').val(),
+        //exp_month: $('.card-expiry-month').val(),
+        //exp_year: $('.card-expiry-year').val()
+    //}, stripeResponseHandler);
 
     // prevent the form from submitting with the default action
-    return false;
-  });
-});
-
+//    return false;
+  //});
+//});
 
